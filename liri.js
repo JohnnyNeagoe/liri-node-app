3baa263d6031c90bea99ca61fb306ea65111d90f
Johnny Neagoe <johnnyneagoe@gmail.com>
2018-07-26 22:46:58 -0400

    03

--- liri.js
+++ liri.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 var fs = require('fs');
 var keys = require('./keys')
 var Twitter = require('twitter');
-var myTweets;
- 
+var listTweets;
+var spotifySearch;
 myTweets();
+spotifyThisSong()
  
 function myTweets(){
     var params = { 
@@ -13,19 +14,22 @@ function myTweets(){
     };
     var client = new Twitter(keys.twitter);
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-       for (var i = 0; i<tweets.length;i++){
-           var myTweets = ('\n' + "@" + params.screen_name + "\n" + "tweeted: " + tweets[i].text + "\n" + "on :" + tweets[i].created_at)
-           console.log(myTweets);
+        if (!error) {
+            for (var i = 0; i<tweets.length;i++){
+                var listTweets = ('\n' + "@" + params.screen_name + "\n" + "tweeted: " + tweets[i].text + "\n" + "on :" + tweets[i].created_at)
+                console.log(listTweets);
 
-       }
-        //console.log(JSON.stringify(tweets, null, 2));
-    }
+            }
+        }
     });
+}
+
+function spotifyThisSong(){
+    search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+    var spotify = new Spotify(keys.spotify);
 
 }
 
-//var spotify = new Spotify(keys.spotify);
 
 
 //console.log(spotify);
